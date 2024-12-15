52a0047569e052ebf5dde2d51ad49b3badcc3bb7
Hesham749 <heesham749@gmail.com>
2024-12-15 22:09:04 +0200

    optimize nested loop performance

--- C#/Day 3/Lab/day3/day3.demo1/Program.cs
+++ C#/Day 3/Lab/day3/day3.demo1/Program.cs
@@ -6,7 +6,7 @@ namespace day3.max
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 3, 3, 4, 3, 5, 7 };
+            int[] arr = { 1, 2, 2, 8, 8, 8, 7 };
             int maxDistance = 0;
             int[] freq = new int[100];
             Stopwatch sp = new Stopwatch();
@@ -24,32 +24,37 @@ namespace day3.max
                             maxDistance = j - i;
                         }
                     }
+
                 }
             }
             sp.Stop();
             Console.WriteLine(sp.Elapsed);
+            sp.Reset();
 
             Console.WriteLine($"max distance is {maxDistance}");
 
             //2
+            maxDistance = 0;
             sp.Start();
             for (int i = 0; i < arr.Length; i++)
             {
                 if (freq[arr[i]] == 0)
                     freq[arr[i]] = i + 1;
-                if (i - (freq[arr[i]] - 1) > maxDistance && freq[arr[i]] != 0)
+                if (i - (freq[arr[i]] - 1) > maxDistance)
                     maxDistance = i - (freq[arr[i]] - 1);
             }
             sp.Stop();
             Console.WriteLine(sp.Elapsed);
+            sp.Reset();
             Console.WriteLine($"max distance is {maxDistance}");
 
             // 3
+            maxDistance = 0;
             sp.Start();
             for (int i = 0; i < arr.Length; i++)
             {
 
-                for (int j = arr.Length - 1; j >= 0; j--)
+                for (int j = arr.Length - 1; j >= i; j--)
                 {
 
                     if (arr[i] == arr[j])
@@ -58,6 +63,8 @@ namespace day3.max
                         {
                             maxDistance = j - i;
                         }
+                        // because in that case j is the largest distance possible for that number
+                        break;
                     }
                 }
             }
