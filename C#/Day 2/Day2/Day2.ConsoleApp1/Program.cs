63fee59e6fcaa1f01ae8ecc35bde1f5942711009
Hesham749 <heesham749@gmail.com>
2024-12-18 21:52:30 +0200

    refactor Program.cs to remove commented code and add reference variable demonstration

--- C#/Day 2/Day2/Day2.ConsoleApp1/Program.cs
+++ C#/Day 2/Day2/Day2.ConsoleApp1/Program.cs
@@ -17,7 +17,7 @@ namespace Day2.ConsoleApp1
             y.Add(x);
             y.Add(2);
             int t = (int)y[0];
-            Console.WriteLine((int)y[0] + (int)x);
+            //Console.WriteLine((int)y[0] + (int)x);
             //int z = x;
             string v = "Hesham";
             switch (v)
@@ -25,6 +25,10 @@ namespace Day2.ConsoleApp1
                 case "Hesham":
                     break;
             }
+            int xx = 20;
+            ref readonly int s = ref t;
+            s = ref xx;
+            Console.WriteLine(s);
         }
     }
 }
