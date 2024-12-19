4b95e7671d90052c75e9f66ed2b64afe39b936cc
Hesham749 <heesham749@gmail.com>
2024-12-19 17:20:49 +0200

    u

--- C#/Day 3/Lab/day3/12ex/_12ex.ex5/Program.cs
+++ C#/Day 3/Lab/day3/12ex/_12ex.ex5/Program.cs
@@ -7,9 +7,9 @@
             int numberOFXBesideCenter = 0;
             for (int i = 0; i < 20; i++)
             {
-                for (int j = 0; j < 40; j++)
+                for (int j = 0; j < 39; j++)
                 {
-                    if (j >= (20 - numberOFXBesideCenter) && j <= (20 + numberOFXBesideCenter))
+                    if (j >= (19 - numberOFXBesideCenter) && j <= (19 + numberOFXBesideCenter))
                         Console.Write("X");
                     else
                         Console.Write(" ");
