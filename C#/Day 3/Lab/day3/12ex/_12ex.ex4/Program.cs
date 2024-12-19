9c220c4eca00bf338c4bf49b1391a6e1e38ff7fb
Hesham749 <heesham749@gmail.com>
2024-12-19 16:22:37 +0200

    Add new project _12ex.ex4 with basic arithmetic console app
    A new project named _12ex.ex4 has been added to the solution _12ex.sln:
    - The project _12ex.ex4 has been added to the solution file _12ex.sln with the project GUID {47C45FB8-9059-4C6B-9C2C-0F963E4A9B93}.
    - Configuration settings for _12ex.ex4 have been added to the GlobalSection(SolutionConfigurationPlatforms) in _12ex.sln, including Debug and Release configurations for Any CPU.
    
    A new C# file Program.cs has been added to the _12ex.ex4 project. This file contains a simple console application that performs basic arithmetic operations (addition, subtraction, multiplication, division) based on user input and allows the user to perform multiple calculations in a loop.
    
    A new project file _12ex.ex4.csproj has been created for the _12ex.ex4 project. This file specifies that the project uses the .NET 8.0 framework and includes settings for implicit usings and nullable reference types.

---
+++ C#/Day 3/Lab/day3/12ex/_12ex.ex4/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _12ex.ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, result;
+            char op, newCalc = 'n';
+
+            do
+            {
+                Console.Clear();
+                Console.Write("please enter first number : ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.Write("please enter operation : ");
+                op = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Write("please enter second number : ");
+                num2 = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                    default:
+                        result = 0;
+                        break;
+                }
+                Console.WriteLine($"answer = {result}");
+                Console.Write("Do another (y/n) ? ");
+                newCalc = Console.ReadKey().KeyChar;
+            } while (newCalc == 'y' || newCalc == 'Y');
+        }
+    }
+}
