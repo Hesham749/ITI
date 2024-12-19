6050995cdb5fcbbf30613b470676f9a89a651537
Hesham749 <heesham749@gmail.com>
2024-12-19 13:22:42 +0200

    Add new C# project with table generation functionality
    Introduce new Visual Studio solution `_12ex.sln` for project `_12ex.ex1` configured for VS 17. Add `Program.cs` with `Main` method calling `GenereteTableStartFromNum(7)` to generate and print a 3x10 table. Include `_12ex.ex1.csproj` targeting .NET 8.0 framework.

---
+++ C#/Day 3/Lab/day3/12ex/12ex.ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _12ex.ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenereteTableStartFromNum(7);
+        }
+
+        private static void GenereteTableStartFromNum(int num)
+        {
+            int[,] nums = new int[3, 10];
+            int current = 0;
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+                    current += num;
+                    nums[i, j] = current;
+                    Console.Write($"{nums[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
