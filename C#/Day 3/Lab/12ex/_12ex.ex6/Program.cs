8632c59257a0bf5b98335ad78e4e20fea5d837ac
Hesham749 <heesham749@gmail.com>
2024-12-19 22:49:24 +0200

    Add new project _12ex.ex6 with factorial calculation
    - Added new project _12ex.ex6 to the solution file _12ex.sln, including build configurations for Debug and Release modes.
    - Created Program.cs under namespace _12ex.ex6 to calculate the factorial of a given number, with an option to repeat or exit by pressing '0'.
    - Added _12ex.ex6.csproj targeting .NET 8.0, with implicit usings and nullable reference types enabled.

---
+++ C#/Day 3/Lab/12ex/_12ex.ex6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _12ex.ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, factorial;
+            char again;
+
+            do
+            {
+                Console.Clear();
+                factorial = 1;
+                Console.WriteLine("please enter number to calculate its factorial");
+                num = int.Parse(Console.ReadLine());
+                for (int i = 2; i <= num; i++)
+                    factorial *= i;
+                Console.WriteLine($"factorial = {factorial}");
+                Console.Write("press any key to continue or press 0 to exit : ");
+                again = Console.ReadKey().KeyChar;
+            } while (again != '0');
+        }
+    }
+}
