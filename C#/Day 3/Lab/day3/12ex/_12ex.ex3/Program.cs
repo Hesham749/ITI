c87e4bca4b7cd7cec95ffeca8ab0e31c9223d89c
Hesham749 <heesham749@gmail.com>
2024-12-19 15:26:43 +0200

    Add new project _12ex.ex3 to solution _12ex.sln
    - Updated _12ex.sln to include new project _12ex.ex3
    - Added build configurations for Debug and Release modes
    - Created Program.cs with a console app for user input
    - Created _12ex.ex3.csproj targeting .NET 8.0 with implicit usings and nullable reference types

---
+++ C#/Day 3/Lab/day3/12ex/_12ex.ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _12ex.ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number up to six digits");
+            int num = 0;
+            char digit;
+            for (int i = 0; i < 6; i++)
+            {
+                digit = Console.ReadKey().KeyChar;
+                if (digit == 13)
+                    break;
+
+                num *= 10;
+                num += (digit - 48);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(num);
+        }
+    }
+}
