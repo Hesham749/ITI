0bcbbb165cffdce751bc5a823273bf8cb840916a
Hesham749 <heesham749@gmail.com>
2024-12-20 00:26:55 +0200

    Add new project _12ex.ex7 with interest calculation
    A new project named _12ex.ex7 has been added to the solution _12ex.sln. This includes the project details in the solution file and configurations for Debug and Release builds.
    
    The new namespace _12ex.ex7 in Program.cs contains a Program class with a Main method that calculates the amount of money after a certain number of years given an initial amount, number of years, and interest rate.
    
    A new project file _12ex.ex7.csproj has been added, specifying the project SDK, output type, target framework (net8.0), and enabling implicit usings and nullable reference types.

---
+++ C#/Day 3/Lab/12ex/_12ex.ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _12ex.ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intitialAmount, numOfYears;
+            float interestRate, moneyAfter;
+            Console.Write("Enter initial amount : ");
+            intitialAmount = int.Parse(Console.ReadLine());
+            Console.Write("Enter number of years : ");
+            numOfYears = int.Parse(Console.ReadLine());
+            Console.Write("Enter interest Rate (percent per year) : ");
+            interestRate = float.Parse(Console.ReadLine());
+            moneyAfter = intitialAmount;
+            for (int i = 0; i < numOfYears; i++)
+                moneyAfter *= (interestRate / 100f) + 1;
+            Console.WriteLine($"At the end of {numOfYears} years, you will have {Math.Round(moneyAfter, 2)} dollars.");
+        }
+    }
+}
