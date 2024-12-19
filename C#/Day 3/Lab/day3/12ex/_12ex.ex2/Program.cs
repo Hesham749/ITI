757a7bbff0d20b925aa33b67a6236a217c84d8fb
Hesham749 <heesham749@gmail.com>
2024-12-19 14:12:10 +0200

    Add new project _12ex.ex2 for temperature conversion
    A new project named `_12ex.ex2` has been added to the Visual Studio solution file `_12ex.sln`. This includes the project configuration for both Debug and Release modes.
    
    A new `Program.cs` file has been added to the `_12ex.ex2` project. This file contains a program that allows the user to convert temperatures between Fahrenheit and Celsius based on their input.
    
    A new project file `_12ex.ex2.csproj` has been created for the `_12ex.ex2` project. This file specifies that the project uses the .NET SDK, targets .NET 8.0, and has implicit usings and nullable reference types enabled.

---
+++ C#/Day 3/Lab/day3/12ex/_12ex.ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _12ex.ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type 1 to convert Fahrenheit to Celsius\n    2 to convert Celsius to Fahrenheit :");
+            byte choise = byte.Parse(Console.ReadLine());
+            if (choise == 1 || choise == 2)
+            {
+                float temp;
+                string from = (choise == 1) ? "Fahrenheit" : "Celsius";
+                string to = (choise == 1) ? "Celsius" : "Fahrenheit";
+
+                Console.Write($"Enter temperature in {from} :");
+                temp = float.Parse(Console.ReadLine());
+                temp = (choise == 1) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+                Console.WriteLine($"in {to} that's {temp}");
+            }
+            else
+                Console.WriteLine("Wrong choice");
+        }
+    }
+}
