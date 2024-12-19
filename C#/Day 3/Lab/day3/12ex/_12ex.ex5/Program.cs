059ddb4243d743519f6c66e61c03b29aa58c3637
Hesham749 <heesham749@gmail.com>
2024-12-19 17:02:11 +0200

    Refactor Main method to add nested loop for pattern
    Refactored the Main method in the Program class:
    - Removed initialization of `numberOFXBesideCenter` to 0.
    - Added nested loops: outer loop runs 20 times, inner loop runs 40 times.
    - Inner loop prints "X" or space based on column index `j` and `numberOFXBesideCenter`.
    - Incremented `numberOFXBesideCenter` by 2 after each row and printed a new line.

--- C#/Day 3/Lab/day3/12ex/_12ex.ex5/Program.cs
+++ C#/Day 3/Lab/day3/12ex/_12ex.ex5/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-
+            int numberOFXBesideCenter = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    if (j >= (20 - numberOFXBesideCenter/2) && j <= (20 + numberOFXBesideCenter/2))
+                        Console.Write("X");
+                    else
+                        Console.Write(" ");
+                }
+                numberOFXBesideCenter += 2;
+                Console.WriteLine();
+            }
         }
     }
 }
