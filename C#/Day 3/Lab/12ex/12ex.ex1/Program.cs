406c78e5d26cde0dc5fb42adb9eded9db57c5648
Hesham749 <heesham749@gmail.com>
2024-12-20 02:02:50 +0200

    Refactor number printing in GenerateTableStartFromNum
    Updated Console.Write to format output with left-aligned width of 4 characters.
    Removed nums array and its assignments, using only the current variable for printing.

--- C#/Day 3/Lab/12ex/12ex.ex1/Program.cs
+++ C#/Day 3/Lab/12ex/12ex.ex1/Program.cs
@@ -10,16 +10,13 @@ namespace _12ex.ex1
 
         private static void GenereteTableStartFromNum(int num)
         {
-            int[,] nums = new int[20, 10];
             int current = 0;
             for (int i = 0; i < 20; i++)
             {
-
                 for (int j = 0; j < 10; j++)
                 {
                     current += num;
-                    nums[i, j] = current;
-                    Console.Write($"{nums[i, j]}\t");
+                    Console.Write($"{current,-4}\t");
                 }
                 Console.WriteLine();
             }
