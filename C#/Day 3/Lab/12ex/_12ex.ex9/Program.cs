d8b0a868e6196207f5da9e156c2498bd2f820394
Hesham749 <heesham749@gmail.com>
2024-12-20 16:12:01 +0200

    ex9

---
+++ C#/Day 3/Lab/12ex/_12ex.ex9/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _12ex.ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char calcAgain;
+            int numOfChairs, numOfGuests, numOfArrangement, factorialOfChairs, factorialOfGuests;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Enter the number of guests : ");
+                numOfGuests = int.Parse(Console.ReadLine());
+                Console.Write("Enter the number of Chairs : ");
+                numOfChairs = int.Parse(Console.ReadLine());
+
+                factorialOfGuests = GetFactorial(numOfGuests);
+                factorialOfChairs = GetFactorial(numOfGuests - numOfChairs);
+                numOfArrangement = factorialOfGuests / factorialOfChairs;
+                Console.WriteLine($"Num of arrangement = {numOfArrangement}");
+                Console.Write("Do you want to continue (y/n) : ");
+                calcAgain = Console.ReadKey().KeyChar;
+            } while (calcAgain == 'y' || calcAgain == 'Y');
+        }
+        static int GetFactorial(int num)
+        {
+            int factorial = num;
+            for (int i = 2; i < num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
