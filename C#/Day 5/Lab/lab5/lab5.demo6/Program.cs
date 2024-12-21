f8399c57b6ceba4320bf8d47ed11a8ced16d8007
Hesham749 <heesham749@gmail.com>
2024-12-21 21:29:23 +0200

    clsComplex

---
+++ C#/Day 5/Lab/lab5/lab5.demo6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace lab5.demo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsComplex x1 = new clsComplex(1, -2);
+            x1.Print();
+            x1.Img = 100;
+            x1.Print();
+        }
+    }
+}
---
+++ C#/Day 5/Lab/lab5/lab5.demo6/clsComplex.cs
@@ -0,0 +1,25 @@
+﻿namespace lab5.demo6
+{
+    internal class clsComplex
+    {
+        public double Real
+        {
+            private get; set;
+        }
+        public double Img
+        {
+            private get; set;
+        }
+
+        public clsComplex(double real, double img)
+        {
+            Real = real;
+            Img = img;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Real}{(Img < 0 ? "" : "+")}{Img}j");
+        }
+    }
+}
