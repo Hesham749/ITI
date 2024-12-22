996c0821781f9e7c6a68f9fc65e5b039b4595898
Hesham749 <heesham749@gmail.com>
2024-12-22 18:23:19 +0200

    complex

--- C#/Day 5/Lab/lab5/lab5.demo6/Program.cs
+++ C#/Day 5/Lab/lab5/lab5.demo6/Program.cs
@@ -4,10 +4,15 @@
     {
         static void Main(string[] args)
         {
-            clsComplex x1 = new clsComplex(1, -2);
+            clsComplex x1 = new clsComplex();
             x1.Print();
             x1.Img = 100;
             x1.Print();
+            clsComplex x2 = new clsComplex(1, -1);
+            x2.Print();
+            x2.SetComplex(5);
+            x2.Print();
+            Console.WriteLine(clsComplex.Counter);
         }
     }
 }
--- C#/Day 5/Lab/lab5/lab5.demo6/clsComplex.cs
+++ C#/Day 5/Lab/lab5/lab5.demo6/clsComplex.cs
@@ -2,24 +2,33 @@
 {
     internal class clsComplex
     {
+        static public int Counter { get; private set; }
+
         public double Real
         {
-            private get; set;
+            get; set;
         }
         public double Img
         {
-            private get; set;
+            get; set;
         }
 
-        public clsComplex(double real, double img)
+        public clsComplex(double real = 0, double img = 0)
         {
+            Counter++;
             Real = real;
             Img = img;
         }
 
+        public void SetComplex(double real, double img = 0)
+        {
+            Real = real;
+            Img = img != 0 ? img : Img;
+        }
+
         public void Print()
         {
-            Console.WriteLine($"{Real}{(Img < 0 ? "" : "+")}{Img}j");
+            Console.WriteLine($"{Real}{(Img <= 0 ? "" : "+")}{(Img == 0 ? "" : $"{Img}j")}");
         }
     }
 }
