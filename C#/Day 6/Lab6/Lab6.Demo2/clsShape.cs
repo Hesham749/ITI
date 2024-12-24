2765538aa8f0f7a6806c23adbb0247695364b8d0
Hesham749 <heesham749@gmail.com>
2024-12-24 23:50:47 +0200

    shape

--- C#/Day 6/Lab6/Lab6.Demo2/clsShape.cs
+++ C#/Day 6/Lab6/Lab6.Demo2/clsShape.cs
@@ -1,9 +1,28 @@
 ﻿namespace Lab6.Demo2
 {
-    internal class clsShape
+    internal abstract class clsShape
     {
         public int Diminsion1 { get; protected set; }
         public int Diminsion2 { get; protected set; }
 
+        public virtual bool SetDiminsion1(int dim)
+        {
+            if (dim < 1 || dim == Diminsion2)
+                return false;
+            Diminsion1 = dim;
+            return true;
+        }
+
+        public virtual bool SetDiminsion2(int dim)
+        {
+            if (dim < 1 || dim == Diminsion1)
+                return false;
+            Diminsion2 = dim;
+            return true;
+        }
+
+        public abstract float GetArea();
+
+        public abstract void Print();
     }
 }
