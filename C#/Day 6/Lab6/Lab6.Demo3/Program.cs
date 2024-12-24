8d6e4f183dc79dfbbf799a65f13015c6e5af350c
Hesham749 <heesham749@gmail.com>
2024-12-25 00:49:29 +0200

    strComplex

---
+++ C#/Day 6/Lab6/Lab6.Demo3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab6.Demo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            strComplex x1 = new strComplex();
+            x1.Print();
+            x1.Img = 100;
+            x1.Print();
+            strComplex x2 = new strComplex(1, -1);
+            x2.Print();
+            x2.SetComplex(5);
+            x2.Print();
+        }
+    }
+}
---
+++ C#/Day 6/Lab6/Lab6.Demo3/strComplex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Demo3
+{
+    internal struct strComplex
+    {
+        public double Real
+        {
+            get; set;
+        }
+        public double Img
+        {
+            get; set;
+        }
+
+        public strComplex()
+        {
+            Real = 0;
+            Img = 0;
+        }
+
+        public strComplex(double real = 0, double img = 0)
+        {
+
+            Real = real;
+            Img = img;
+        }
+
+        public void SetComplex(double real, double img = 0)
+        {
+            Real = real;
+            Img = img != 0 ? img : Img;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Real}{(Img <= 0 ? "" : "+")}{(Img == 0 ? "" : $"{Img}j")}");
+        }
+    }
+}
