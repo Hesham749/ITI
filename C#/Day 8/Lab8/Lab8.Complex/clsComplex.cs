033d0dd7ee924927eef997ce42c5196520946982
Hesham749 <heesham749@gmail.com>
2024-12-25 15:30:54 +0200

    override equals

--- C#/Day 8/Lab8/Lab8.Complex/clsComplex.cs
+++ C#/Day 8/Lab8/Lab8.Complex/clsComplex.cs
@@ -17,12 +17,6 @@ namespace Lab8.Complex
             get; set;
         }
 
-        public clsComplex()
-        {
-            Real = 0;
-            Img = 0;
-        }
-
         public clsComplex(double real = 0, double img = 0)
         {
 
@@ -34,6 +28,20 @@ namespace Lab8.Complex
         {
             Real = real;
             Img = img != 0 ? img : Img;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (typeof(clsComplex) != obj.GetType()) return false;
+            clsComplex c = (clsComplex)obj;
+            if (c.Real != Real && Img != c.Img) return false;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Real}{(Img <= 0 ? "" : "+")}{(Img == 0 ? "" : $"{Img}j")}";
         }
 
         public void Print()
