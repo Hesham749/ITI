c44e97be45a5fa3ab8c396dd86feadc13c546eda
Hesham749 <heesham749@gmail.com>
2024-12-25 21:46:03 +0200

    u

--- C#/Day 8/Lab8/Lab8.Duration/Program.cs
+++ C#/Day 8/Lab8/Lab8.Duration/Program.cs
@@ -5,24 +5,39 @@
         static void Main(string[] args)
         {
             clsDuration d0 = new clsDuration(1, 30, 20);
-            //Console.WriteLine(d0);
+            Console.WriteLine(d0);
+
             var d = new clsDuration(1, 10, 15);
-            //Console.WriteLine(d);
+            Console.WriteLine(d);
+
             var d1 = new clsDuration(3600);
             Console.WriteLine(d1);
+
             var d2 = new clsDuration(7800);
             Console.WriteLine(d2);
+
             var d3 = new clsDuration(666);
-            //Console.WriteLine(d3);
+            Console.WriteLine(d3);
+
             d3 = d1 + 7800;
             Console.WriteLine(d3);
+
             d3 = 666 + d3;
             Console.WriteLine(d3);
+
             d3 = d1++;
             Console.WriteLine(d3);
+
             d3 = --d2;
             Console.WriteLine(d3);
+
             Console.WriteLine(d1 - d2);
+
+            if (d1 > d2)
+                Console.WriteLine($"{d1} > {d2}");
+
+            if (d1 <= d2)
+                Console.WriteLine($"{d1} <= {d2}");
         }
     }
 }
