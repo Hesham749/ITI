95d575461688021a11968ceefa3d3552407ec919
Hesham749 <heesham749@gmail.com>
2024-12-25 15:18:31 +0200

    clsComplex start

---
+++ C#/Day 8/Lab8/Lab8.Complex/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab8.Complex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ C#/Day 8/Lab8/Lab8.Complex/clsComplex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.Complex
+{
+    internal class clsComplex
+    {
+        public double Real
+        {
+            get; set;
+        }
+        public double Img
+        {
+            get; set;
+        }
+
+        public clsComplex()
+        {
+            Real = 0;
+            Img = 0;
+        }
+
+        public clsComplex(double real = 0, double img = 0)
+        {
+
+            Real = real;
+            Img = img;
+        }
+
+        public void SetComplex(double real, double img = 0)
+        {
+            Real = real;
+            Img = img != 0 ? img : Img;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Real}{(Img <= 0 ? "" : "+")}{(Img == 0 ? "" : $"{Img}j")}");
+        }
+    }
+}
