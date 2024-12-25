278906d449f313203e5546c46a749fa956a10246
Hesham749 <heesham749@gmail.com>
2024-12-26 00:11:50 +0200

    simplify method

--- C#/Day 8/Lab8/Lab8.Fraction/Program.cs
+++ C#/Day 8/Lab8/Lab8.Fraction/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Lab8.Fraction;
+
+clsFraction f1 = new clsFraction(32, 24);
+Console.WriteLine(f1);
+Console.WriteLine(f1.Equals(new clsFraction(1, 3)));
+Console.WriteLine(clsFraction.Simplify(f1));
--- C#/Day 8/Lab8/Lab8.Fraction/clsFraction.cs
+++ C#/Day 8/Lab8/Lab8.Fraction/clsFraction.cs
@@ -9,6 +9,7 @@
         {
             Denominator = 1;
             Denominator = denominator;
+            Numerator = numerator;
         }
 
         public bool SetDenominator(int denominator)
@@ -19,6 +20,24 @@
             return true;
         }
 
+        public static clsFraction Simplify(clsFraction f)
+        {
+            int small = ((f.Numerator < f.Denominator) ? f.Numerator : f.Denominator);
+            int commonFactor = 0;
+            for (int i = 2; i <= small / 2; i++)
+            {
+                if (f.Denominator % i == 0 && f.Numerator % i == 0)
+                {
+                    commonFactor = i;
+                }
+                if (f.Denominator % (small - i) == 0 && f.Numerator % (small - 100) == 0)
+                {
+                    commonFactor = i;
+                    break;
+                }
+            }
+            return new clsFraction(f.Numerator / commonFactor, f.Denominator / commonFactor);
+        }
 
         public override bool Equals(object? obj)
         {
