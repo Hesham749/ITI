41842a2dd1c302d9a5320d32b56345438778f84e
Hesham749 <heesham749@gmail.com>
2024-12-25 22:51:17 +0200

    clsFraction Creation

---
+++ C#/Day 8/Lab8/Lab8.Fraction/clsFraction.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab8.Fraction
+{
+    internal class clsFraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; protected set; }
+
+        public clsFraction(int numerator, int denominator)
+        {
+            Denominator = 1;
+            Denominator = denominator;
+        }
+
+        public bool SetDenominator(int denominator)
+        {
+            if (denominator == 0)
+                return false;
+            Denominator = denominator;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+    }
+}
