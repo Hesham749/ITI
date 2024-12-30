f1e6b856f35fe3552119de2b95435a607ad2c33e
Hesham749 <heesham749@gmail.com>
2024-12-30 15:49:31 +0200

    Swap generic function

---
+++ C#/Lab10/Lab10.Demo1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab10.Demo1
+{
+    internal class Program
+    {
+
+        static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 5, y = 3;
+            Console.WriteLine($"x = {x} , y = {y}");
+            Swap(ref x, ref y);
+            Console.WriteLine($"x = {x} , y = {y}");
+        }
+    }
+}
