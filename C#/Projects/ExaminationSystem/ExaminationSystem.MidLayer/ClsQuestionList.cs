78e51354e4e0687126c527cca281d3768f063cef
Hesham749 <heesham749@gmail.com>
2025-01-04 00:49:05 +0200

    Add ClsQuestionList class with JSON handling
    Introduce ClsQuestionList in ExaminationSystem.MidLayer.
    - Inherits from List<ClsQuestion>
    - Maintains static count with _questionListCount
    - Stores JSON file name in _fileName
    - Unique Id property for each instance
    - Constructor initializes Id and _fileName, loads questions
    - Override Add method to add questions
    - GetQuestionList method to deserialize JSON file
    - Added necessary using directives

---
+++ C#/Projects/ExaminationSystem/ExaminationSystem.MidLayer/ClsQuestionList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem.MidLayer
+{
+    internal class ClsQuestionList : List<ClsQuestion>
+    {
+        private static int _questionListCount;
+        private string _fileName;
+        public int Id
+        {
+            get; private set;
+        }
+        public ClsQuestionList()
+        {
+            Id = ++_questionListCount;
+            _fileName = $"QuestionList{Id}.json";
+            ClsQuestionList questions = GetQuestionList();
+            if (questions?.Count > 0)
+            {
+                foreach (var question in questions)
+                    Add(question);
+            }
+        }
+
+        public new void Add(ClsQuestion question)
+        {
+            base.Add(question);
+        }
+
+        ClsQuestionList GetQuestionList()
+        {
+            string jsonString = File.ReadAllText(_fileName);
+            if (jsonString == "") return null;
+            return JsonSerializer.Deserialize<ClsQuestionList>(jsonString);
+        }
+
+    }
+}
