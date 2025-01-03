3fa05efcfeb5a7d3d3535827c30ae953bed9651e
Hesham749 <heesham749@gmail.com>
2025-01-03 23:10:55 +0200

    start point

---
+++ C#/Projects/ExaminationSystem/ExaminationSystem.MidLayer/ClsQuestion.cs
@@ -0,0 +1,14 @@
+﻿namespace ExaminationSystem.MidLayer
+{
+    public enum EnQuestionType
+    {
+        TrueFalse,
+        ChooseOne,
+        ChooseMultiple
+
+    }
+    public abstract class ClsQuestion
+    {
+
+    }
+}
