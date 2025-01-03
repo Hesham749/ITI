d80d71009aa72be6e01f001dd41282069f54a41d
Hesham749 <heesham749@gmail.com>
2025-01-04 00:04:32 +0200

    ClsChooseMultiple

---
+++ C#/Projects/ExaminationSystem/ExaminationSystem.MidLayer/ClsChooseMultiple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem.MidLayer
+{
+    internal class ClsChooseMultiple : ClsQuestion
+    {
+        public ClsChooseMultiple(string body)
+        {
+            QuestionType = EnQuestionType.ChooseMultiple;
+            Mark = 2;
+            Header = "Read each question carefully and select all correct answers :";
+            Body = body;
+        }
+    }
+}
