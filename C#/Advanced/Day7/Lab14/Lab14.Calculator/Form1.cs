efbc2cf94fced31c078d0974bd4011953a1d4bf6
Hesham749 <heesham749@gmail.com>
2025-01-06 01:56:46 +0200

    Refactor UI and add key press event handling
    - Adjusted `Margin`, `Size`, and `Location` properties for various controls.
    - Added `KeyPress` event handlers for multiple buttons.
    - Modified `Num_click` and `InsertBtnTextAtPosition` methods to improve text insertion.
    - Simplified `GetInsertPosition` method.
    - Commented out `btnDel_Click` method.
    - Added new methods for key press handling, preventing invalid entries, and performing operations.
    - Introduced static variables `op` and `num1` for operation handling.
    - Added readonly arrays `acceptedInput` and `mathOperations` for input validation.

--- C#/Advanced/Day7/Lab14/Lab14.Calculator/Form1.cs
+++ C#/Advanced/Day7/Lab14/Lab14.Calculator/Form1.cs
@@ -13,22 +13,26 @@ namespace Lab14.Calculator
         {
             Button b = sender as Button;
             if (b != null)
-                InsertBtnTextAtPosition(b);
+                InsertBtnTextAtPosition(b.Text);
         }
 
-        private void InsertBtnTextAtPosition(Button b)
+        private void InsertBtnTextAtPosition(string b)
         {
+            txtResult.Focus();
             int pos = GetInsertPosition();
-            if (!txtResult.Text.Contains('.') || b != btnDot)
-                txtResult.Text = txtResult.Text.Insert(pos, b.Text);
-            else if (txtResult.Text.Contains(".") && b == btnDot)
+            if (!txtResult.Text.Contains('.') || b == ".")
+                txtResult.Text = txtResult.Text.Insert(pos, b);
+            else if (txtResult.Text.Contains(".") && b == ".")
                 SystemSounds.Exclamation.Play();
+            txtResult.SelectionStart = pos + 1;
+
         }
 
         private int GetInsertPosition()
         {
             int pos = 0;
-            pos = !txtResult.Focused ? txtResult.Text.Length : txtResult.SelectionStart;
+            pos = txtResult.Focused ? txtResult.SelectionStart : txtResult.Text.Length;
+
             return pos;
         }
 
@@ -41,6 +45,89 @@ namespace Lab14.Calculator
         {
             //if (txtResult.Text.Length > 0 && txtResult.SelectionStart > 0)
             //txtResult.Text = txtResult.Text.Remove(GetInsertPosition() - 1, txtResult.SelectionLength);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private static void PreventEntry(KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            SystemSounds.Exclamation.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        static string op;
+        static float num1 = 0;
+
+        readonly static string[] acceptedInput = [".", "+", "-", "*", "X", "/", "÷", "%", "="];
+        readonly static string[] mathOperations = acceptedInput[1..];
+        private void btn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var b = e.KeyChar.ToString();
+            int pos = GetInsertPosition();
+            if ((!char.IsDigit(char.Parse(b)) && b != "\b") && !Array.Exists(acceptedInput, x => x == b))
+                PreventEntry(e);
+            else if (Array.Exists(mathOperations, x => x == b))
+            {
+                num1 = float.Parse(txtResult.Text);
+                op = b;
+                txtResult.Clear();
+            }
+            else if (b == "\b")
+            {
+                txtResult.Text = txtResult.Text.Remove(pos - 1, 1);
+            }
+            else
+            {
+                InsertBtnTextAtPosition(b);
+            }
+            var btn = sender as Button;
+            if (btn != null)
+                btn.Focus();
+        }
+
+        void MakeOp(float currentNum)
+        {
+            switch (op)
+            {
+                case "+":
+                    num1 += currentNum;
+                    break;
+                case "-":
+                    num1 -= currentNum;
+                    break;
+                case "*":
+                case "X":
+                    num1 *= currentNum;
+                    break;
+                case "/":
+                    num1 /= currentNum;
+                    break;
+                case "%":
+                case "÷":
+                    num1 %= currentNum;
+                    break;
+                case "=":
+                    btnEqual_Click(btnEqual, EventArgs.Empty);
+                    break;
+            }
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Operation_btn_clik(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            btn_KeyPress(sender, new KeyPressEventArgs(char.Parse(b?.Text)));
         }
     }
 }
