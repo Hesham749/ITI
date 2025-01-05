94045343b7fab12e8ff204785c9ff789c76d7724
Hesham749 <heesham749@gmail.com>
2025-01-05 11:04:05 +0200

    Add Lab14.Indexer project with ClsComplex class and demo
    Updated Lab14.sln to include the new Lab14.Indexer project.
    Created Lab14.Indexer.csproj targeting .NET 8.0 with necessary configurations.
    Added ClsComplex class with Real and Img properties and an indexer.
    Implemented Program.cs to demonstrate ClsComplex usage.

---
+++ C#/Advanced/Day7/Lab14/Lab14.Indexer/ClsComplex.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab14.Indexer
+{
+    internal class ClsComplex
+    {
+        public int Real { get; set; }
+        public int Img { get; set; }
+        public int this[int x]
+        {
+            get
+            {
+                if (x == 0)
+                    return Real;
+                else if (x == 1)
+                    return Img;
+                else throw new IndexOutOfRangeException();
+
+            }
+            set
+            {
+                if (x == 0)
+                    Real = value;
+                else if (x == 1)
+                    Img = value;
+                else throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ C#/Advanced/Day7/Lab14/Lab14.Indexer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab14.Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsComplex c = new ClsComplex() { Real = 30, Img = 10 };
+            Console.WriteLine(c[0]);
+            Console.WriteLine(c[1]);
+            c[1] = 5;
+            Console.WriteLine(c[1]);
+        }
+    }
+}
