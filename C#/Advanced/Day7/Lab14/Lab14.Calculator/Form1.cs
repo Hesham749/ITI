5af6162f1d73ae507d8cfaff50fbbd7f5bcde0c2
Hesham749 <heesham749@gmail.com>
2025-01-05 16:57:13 +0200

    u

--- C#/Advanced/Day7/Lab14/Lab14.Calculator/Form1.cs
+++ C#/Advanced/Day7/Lab14/Lab14.Calculator/Form1.cs
@@ -18,11 +18,18 @@ namespace Lab14.Calculator
 
         private void InsertBtnTextAtPosition(Button b)
         {
-            int pos = !txtResult.Focused ? txtResult.Text.Length : txtResult.SelectionStart;
+            int pos = GetInsertPosition();
             if (!txtResult.Text.Contains('.') || b != btnDot)
                 txtResult.Text = txtResult.Text.Insert(pos, b.Text);
             else if (txtResult.Text.Contains(".") && b == btnDot)
                 SystemSounds.Exclamation.Play();
+        }
+
+        private int GetInsertPosition()
+        {
+            int pos = 0;
+            pos = !txtResult.Focused ? txtResult.Text.Length : txtResult.SelectionStart;
+            return pos;
         }
 
         private void btnClear_Click(object sender, EventArgs e)
@@ -32,7 +39,8 @@ namespace Lab14.Calculator
 
         private void btnDel_Click(object sender, EventArgs e)
         {
-
+            //if (txtResult.Text.Length > 0 && txtResult.SelectionStart > 0)
+            //txtResult.Text = txtResult.Text.Remove(GetInsertPosition() - 1, txtResult.SelectionLength);
         }
     }
 }
