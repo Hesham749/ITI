322b5137e02bc1900383db9bee703733f9c10aef
Hesham749 <heesham749@gmail.com>
2025-01-19 15:47:41 +0200

    Add new .NET 8.0 project with custom IEnumerable implementation
    Introduce a new Visual Studio solution file for Enumerators_Iterators.IEnumerator project.
    Add project file targeting .NET 8.0 with implicit usings and nullable reference types enabled.
    Include Program.cs with a Main method demonstrating custom Intgers class.
    Implement Intgers class with IEnumerable interface for iteration.

---
+++ C#/Advanced/Enumerators Iterators/Enumerators_Iterators/Enumerators_Iterators.IEnumerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Linq;
+using System;
+namespace Enumerators_Iterators.IEnumerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Intgers fiveIntergers = new(1, 2, 3, 4, 5);
+            foreach (var item in fiveIntergers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class Intgers : IEnumerable
+    {
+        int[] _vlaues;
+        public Intgers(params int[] ints)
+        {
+            _vlaues = ints;
+        }
+
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            foreach (var item in _vlaues)
+            {
+                yield return item;
+            }
+        }
+    }
+
+
+}
