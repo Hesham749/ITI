8ee1233693fd841594edf68a555cc60bb8f4abd3
Hesham749 <heesham749@gmail.com>
2025-01-25 16:06:28 +0200

    Implement question insertion and update triggers to enforce course-instructor validation and correct answer integrity

--- Database/Examination System Project/Procs.sql
+++ Database/Examination System Project/Procs.sql
@@ -100,30 +100,29 @@ GO
 
 -- question
 
--- CREATE PROC sp_InsertQuestion
--- @Body VARCHAR(150),
--- @Mark TINYINT ,
--- @CorrectAnswer TINYINT ,
--- @CrsID int ,
--- @TypeID int ,
--- @InsID  int
--- AS
--- BEGIN
+CREATE PROC sp_InsertQuestion
+@Body VARCHAR(150),
+@Mark TINYINT ,
+@CrsID int ,
+@TypeID int ,
+@InsID  int
+AS
+BEGIN
 
---  BEGIN TRY
---         BEGIN TRANSACTION;
+ BEGIN TRY
+        BEGIN TRANSACTION;
 
---         INSERT INTO Question (Body, Mark, CorrectAnswer, CrsID, TypeID, InsID)
---         VALUES ( @Body, @Mark, @CorrectAnswer, @CrsID, @TypeID, @InsID);
+        INSERT INTO Question (Body, Mark, CrsID, TypeID, InsID)
+        VALUES ( @Body, @Mark, @CrsID, @TypeID, @InsID);
 
---         COMMIT TRANSACTION;
---     END TRY
---     BEGIN CATCH
---         ROLLBACK TRANSACTION;
---         PRINT 'question did not insert';
---     END CATCH
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        PRINT 'question did not insert';
+    END CATCH
 
--- END;
+END;
 
 
 -- drop PROC sp_UpdateQuestion
@@ -159,91 +158,3 @@ GO
 -- END;
 
 -- GO
-
-CREATE TRIGGER trg_InsteadOfInsert
-ON question
-INSTEAD OF INSERT
-AS
- BEGIN
-DECLARE  @CrsID INT , @InsID  INT
-
-SELECT  @CrsID = i.CrsID , @InsID = i.InsID  FROM inserted AS i
-
-IF Exists (SELECT ci.CrsID FROM CoursesInstructors AS ci WHERE ci.CrsID = @CrsID AND ci.InsID = @InsID )
-    BEGIN
-        INSERT INTO question(Body, Mark,CorrectAnswer,TypeID , CrsID , InsID)
-        SELECT Body, Mark,CorrectAnswer,TypeID , CrsID , InsID FROM inserted
-    END
-ELSE
-RAISERROR('operation failed',12,1)
-
-END;
-
-
-CREATE TRIGGER trg_AfterUpdate
-ON question
-After UPDATE
-AS
- BEGIN
-DECLARE  @ID INT , @CrsID INT , @InsID  INT
- IF UPDATE(CrsID) OR  UPDATE(InsID)
-    BEGIN
-        SELECT  @ID = ID , @InsID = InsID ,  @CrsID = CrsID   FROM inserted
-        IF NOT  Exists (SELECT ci.CrsID FROM CoursesInstructors AS ci WHERE ci.CrsID = @CrsID AND ci.InsID = @InsID )
-            BEGIN
-               SELECT @CrsID = CrsID , @InsID = InsID  FROM deleted ;
-                UPDATE question
-                SET CrsID = @CrsID , InsID = @InsID
-                WHERE ID =@ID
-            END
-            ELSE
-                RAISERROR('operation failed',12,1)
-    END
-
-
-END;
-
-
-
-CREATE TRIGGER trg_AfterInsertCorrectAnswer
-ON question
-After INSERT
-AS
- BEGIN
-    DECLARE  @ID INT , @CorrectAnswer INT
-     SELECT @ID = ID , @CorrectAnswer = CorrectAnswer FROM inserted
-        IF NOT  Exists (SELECT qa.OptionNum FROM QuestionOptions AS qa WHERE qa.OptionNum = @CorrectAnswer AND qa.QuestionID = @ID )
-            BEGIN
-                SET @CorrectAnswer = NULL ;
-                UPDATE question
-                SET CorrectAnswer = @CorrectAnswer
-                WHERE ID =@ID
-            END
-            ELSE
-            print 'correct answer st to NULL';
-END;
-
-
-
-create TRIGGER trg_AfterUpdateCorrectAnswer
-ON question
-After UPDATE
-AS
- BEGIN
-    DECLARE  @ID INT , @CorrectAnswer INT
-     SELECT @ID = ID , @CorrectAnswer = CorrectAnswer FROM inserted
-
- IF UPDATE(CorrectAnswer)
-    BEGIN
-        IF NOT  Exists (SELECT qa.OptionNum FROM QuestionOptions AS qa WHERE qa.OptionNum = @CorrectAnswer AND qa.QuestionID = @ID )
-            BEGIN
-                     SELECT @CorrectAnswer = CorrectAnswer FROM deleted ;
-                UPDATE question
-                SET CorrectAnswer = @CorrectAnswer
-                WHERE ID =@ID
-            END
-        ELSE
-            print 'your input is not exist in question options';
-    END
-
-END;
