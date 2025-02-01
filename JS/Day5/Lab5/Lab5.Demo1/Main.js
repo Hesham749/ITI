ae5f0f4765fa1481ef4d8ec6f80d7cdca50fdc05
Hesham749 <heesham749@gmail.com>
2025-02-01 18:39:37 +0200

    add To Do List functionality with HTML, CSS, and JavaScript

---
+++ JS/Day5/Lab5/Lab5.Demo1/Main.js
@@ -0,0 +1,21 @@
+var addBtn = document.querySelector("#Add");
+var TodoList = document.querySelector("#ToDoList");
+var newToDo = document.querySelector("#NewToDo");
+var lists = document.querySelectorAll("li");
+
+addBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (newToDo.value != "") {
+        let nTodo = document.createElement("li");
+        nTodo.innerText = "  " +newToDo.value;
+        TodoList.appendChild(nTodo);
+        newToDo.value = "";
+        nTodo.addEventListener("click", function (e) {
+            let checked = "✓ ";
+            !this.classList.contains("Checked") ? this.classList.add("Checked") : this.classList.remove("Checked");
+            this.innerText = this.classList.contains("Checked") ? checked + this.innerText :  this.innerText.slice(1);
+        });
+    }
+});
+
+
