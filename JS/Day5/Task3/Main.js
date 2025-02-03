fb39a5699665f517afe5965970e271a33d36c5f8
Hesham749 <heesham749@gmail.com>
2025-02-03 14:01:14 +0200

    Refactor XMLHttpRequest handling and update table cell text assignment; add cursor pointer on row hover

--- JS/Day5/Task3/Main.js
+++ JS/Day5/Task3/Main.js
@@ -1,6 +1,5 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
-xhr.send();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
         localStorage.setItem('users', xhr.responseText);
@@ -12,17 +11,18 @@ xhr.onreadystatechange = function() {
             var td2 = document.createElement("td");
             var td3 = document.createElement("td");
             var td4 = document.createElement("td");
-            td1.innerHTML = users[i].id;
-            td2.innerHTML = users[i].name;
-            td3.innerHTML = users[i].email;
-            td4.innerHTML = users[i].phone;
-      
+            td1.innerText = users[i].id;
+            td2.innerText = users[i].name;
+            td3.innerText = users[i].email;
+            td4.innerText = users[i].phone;
+
             tr.appendChild(td1);
             tr.appendChild(td2);
             tr.appendChild(td3);
             tr.appendChild(td4);
-        
+
             tableBody.appendChild(tr);
-        }  
+        }
     }
 };
+xhr.send();
