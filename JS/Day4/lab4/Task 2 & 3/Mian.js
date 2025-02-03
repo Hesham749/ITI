212c12e10b7bb211bcff726edaa69e00c2c4a19d
Hesham749 <heesham749@gmail.com>
2025-02-03 15:11:01 +0200

    Add console logs and modify DOM elements for improved debugging and styling

--- JS/Day4/lab4/Task 2 & 3/Mian.js
+++ JS/Day4/lab4/Task 2 & 3/Mian.js
@@ -1,19 +1,23 @@
 
 //! 1
 
+//a
 var images1 = document.images;
 console.log(images1);
 
 var images2 = document.getElementsByTagName('img');
 console.log(images2);
 
-
+//b
 var options = document.getElementsByTagName('option');
 console.log(options);
 
+//c
 //Find all rows for second table in page
 var rows = table.getElementsByTagName('tr');
 console.log(rows);
+
+//d
 // Find all elements that contain class name fontBlue
 var fontblue = document.getElementsByClassName('fontBlue');
   console.log(fontblue);
@@ -22,18 +26,19 @@ var fontblue = document.getElementsByClassName('fontBlue');
 
 
 //! 2
+//a
 var table = document.getElementsByTagName('table')[1];
 var link = table.getElementsByTagName('a')[0];
 
 link.href = 'http://training.com';
 link.innerText = 'Training';
 
-
+//b
 var img = document.getElementsByTagName('img')[1];
 img.style.border = '5px solid red';
 
 
-
+// c
 var form = document.getElementsByTagName('form')[1];
 
 
@@ -45,5 +50,5 @@ function box_checked(){
     }
 }
 
-
+//d
 document.getElementById('example').style.backgroundColor = 'pink';
