7e713d91f44e574d9d37852975d2c73aaeac93af
Hesham749 <heesham749@gmail.com>
2025-02-08 03:20:11 +0200

    Update Department and Student classes with new features
    Updated using directives in Department.cs to include additional namespaces. Changed Dept_Manager and Manager_hireDate properties in Department class to nullable types. Added ToString method to Department class for string representation. Reformatted LName property in ClsStudent class. Changed St_super property in ClsStudent class to nullable type. Added ToString method to ClsStudent class for string representation.

--- ADO.Net/Lab1/BusinessLayer/Entities/Department.cs
+++ ADO.Net/Lab1/BusinessLayer/Entities/Department.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Azure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +14,12 @@ namespace BusinessLayer
         public string Name { get; set; } = string.Empty;
         public string Desc { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
-        public int Dept_Manager { get; set; }
-        public DateOnly Manager_hireDate { get; set; }
+        public int? Dept_Manager { get; set; }
+        public DateOnly? Manager_hireDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID : {Id} , Name : {Name} , Description : {Desc} , Location : {Location} {((Dept_Manager is null) ? "" : $", Department Manager Id : {Dept_Manager}")} {((Manager_hireDate is null) ? "" : $", Manager Hire Date : {Manager_hireDate}")}";
+        }
     }
 }
--- ADO.Net/Lab1/BusinessLayer/Entities/Student.cs
+++ ADO.Net/Lab1/BusinessLayer/Entities/Student.cs
@@ -10,11 +10,15 @@ namespace BusinessLayer
     {
         public int Id { get; set; }
         public string FName { get; set; } = string.Empty;
-        public string LName { get; set; } = string.Empty ;
+        public string LName { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public byte Age { get; set; }
         public int Dept_id { get; set; }
-        public int St_super { get; set; }
+        public int? St_super { get; set; }
 
+        public override string ToString()
+        {
+            return $"ID : {Id} , Name : {FName} {LName} , Address : {Address} , Age : {Age} , Department ID : {Dept_id} {((St_super is null) ? "" : $", Super : {St_super}")}";
+        }
     }
 }
