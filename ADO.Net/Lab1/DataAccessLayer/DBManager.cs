f9bfcc5ee09e0163d5b3244fa94db5eb1dc2e875
Hesham749 <heesham749@gmail.com>
2025-02-08 02:53:46 +0200

    Add new solution with Business and Data Access layers
    Added Lab1.sln with DataAccessLayer and BusinessLayer projects.
    Created BusinessLayer with ClsDepartment, ClsStudent, ClsDepartmentList, and ClsStudentList classes.
    Created DataAccessLayer with ClsDBManager for SQL operations.
    Added AppSetting.json for database configuration.
    Removed a line from test.txt.

---
+++ ADO.Net/Lab1/DataAccessLayer/DBManager.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace DataAccessLayer
+{
+    public static class ClsDBManager
+    {
+        static string connString = string.Empty;
+        static SqlConnection conn;
+        static SqlCommand cmd;
+        static ClsDBManager()
+        {
+            connString = new ConfigurationBuilder().AddJsonFile("AppSetting").Build().GetSection("conn").Value;
+            conn = new(connString);
+            cmd = new("", conn);
+        }
+
+        public static DataTable GetQueryResult(string cmdText)
+        {
+            DataTable dt = new();
+            cmd.CommandText = cmdText;
+            SqlDataAdapter adapter = new(cmd);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        public static int ExecuteNonQuery(string cmdText)
+        {
+            int rowAffected = 0;
+            cmd.CommandText = cmdText;
+            conn.Open();
+            rowAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rowAffected;
+        }
+
+        public static int ExcuteScalar(string cmdText)
+        {
+            int identity = 0;
+            cmd.CommandText = cmdText;
+            cmd.CommandText += "select Scope_Identity()";
+            conn.Open();
+            identity = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+            return identity;
+        }
+    }
+}
