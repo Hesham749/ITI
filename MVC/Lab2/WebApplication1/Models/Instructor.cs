04a73d3d52471205f1ef76e5a06b291a6b105614
Hesham749 <heesham749@gmail.com>
2025-02-15 05:35:12 +0200

    Add ClsInstructor class for instructor management
    Introduces the `ClsInstructor` class in the `WebApplication1.Models` namespace. This class includes properties for instructor details such as `Id`, `Name`, `Image`, `Salary`, `Address`, `Dept_Id`, and `Crs_Id`. It also features navigation properties for `ClsDepartment` and `ClsCourse`, and is mapped to the "Instructor" table in the database using the `[Table("Instructor")]` attribute.

--- MVC/Lab2/WebApplication1/Models/Instructor.cs
+++ MVC/Lab2/WebApplication1/Models/Instructor.cs
@@ -1,6 +1,20 @@
-﻿namespace WebApplication1.Models
+﻿// Ignore Spelling: Crs
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
 {
+    [Table("Instructor")]
     public class ClsInstructor
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public float Salary { get; set; }
+        public string Address { get; set; }
+        public int Dept_Id { get; set; }
+        public int Crs_Id { get; set; }
+        public ClsDepartment Department { get; set; }
+        public ClsCourse Course { get; set; }
     }
 }
