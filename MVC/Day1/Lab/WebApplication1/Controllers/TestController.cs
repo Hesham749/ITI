a98b47f619a9c761f638c5da1e6a006b5d8afd87
Hesham749 <heesham749@gmail.com>
2025-02-17 07:04:08 +0200

    u

---
+++ MVC/Day1/Lab/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAdd()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public string AddStudent(Student st)
+        {
+            return st.ToString();
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public string Display()
+        {
+            return "Welcome";
+        }
+    }
+}
---
+++ MVC/Day1/Lab/WebApplication1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}-{Name}-{Age}";
+        }
+    }
+}
