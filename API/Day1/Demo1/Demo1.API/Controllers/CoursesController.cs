fcc15c8bb7bd7cc8b3fbbec83ad6f12dc07bb399
Hesham749 <heesham749@gmail.com>
2025-02-25 17:10:44 +0200

    Add Demo1 API and TestClient with CRUD functionality
    Created and configured Demo1 and TestClient projects for Visual Studio 2022.
    Added logging and connection settings in appsettings files.
    Set up Demo1 as a .NET 9.0 web application with necessary packages for API documentation and database access.
    Implemented CoursesController for CRUD operations and added model and context definitions.
    Introduced a Windows Forms client to interact with the API, including UI components for course management.

---
+++ API/Day1/Demo1/Demo1.API/Controllers/CoursesController.cs
@@ -0,0 +1,89 @@
+﻿using Demo1.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly Lab1ApiContext context;
+
+        public CoursesController(Lab1ApiContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult get()
+        {
+            List<Course> courses = context.Courses.ToList();
+            if (courses.Count == 0)
+                return NotFound();
+            return Ok(courses);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult deleteCourse(int id)
+        {
+            var crs = context.Courses.Find(id);
+            if (crs is null)
+                return NotFound();
+            context.Remove(crs);
+            context.SaveChanges();
+            List<Course> courses = context.Courses.ToList();
+            return Ok(courses);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult put(int id, Course course)
+        {
+            if (id != course.id)
+                return BadRequest();
+            var crs = context.Courses.AsNoTracking().FirstOrDefault(c => c.id == id);
+            if (crs is null)
+                return NotFound();
+            if (ModelState.IsValid)
+            {
+                context.Update(course);
+                context.SaveChanges();
+                return NoContent();
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        public IActionResult post(Course course)
+        {
+            if (course is null)
+                return BadRequest();
+            if (ModelState.IsValid)
+            {
+                context.Add(course);
+                context.SaveChanges();
+                return Created();
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getById(int id)
+        {
+            var crs = context.Courses.Find(id);
+            if (crs is null)
+                return NotFound();
+            return Ok(crs);
+        }
+        [HttpGet("/api/sts/{name}")]
+        public IActionResult courseByName(string name)
+        {
+            if (name is null)
+                return BadRequest();
+            var crs = context.Courses.FirstOrDefault(c => c.crs_name == name);
+            if (crs is null)
+                return NotFound();
+            return Ok(crs);
+        }
+
+    }
+}
---
+++ API/Day1/TestClient/TestClient/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace TestClient
+{
+    class Course
+    {
+        public int id { get; set; }
+        public string crs_name { get; set; }
+        public string crs_desc { get; set; }
+        public int duration { get; set; }
+    }
+}
