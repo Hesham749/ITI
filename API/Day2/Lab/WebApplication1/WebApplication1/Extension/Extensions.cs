fa1d0da31016227763b73b8ed317d1d98e732148
Hesham749 <heesham749@gmail.com>
2025-02-27 00:14:02 +0200

    Refactor Pagination method for improved logic
    Updated the `Pagination` method in the `Extensions` class to enhance input parameter handling and streamline pagination logic. Consolidated checks for `source`, `pageSize`, and `page` into a single conditional statement. Adjusted maximum page calculation to use `source.Count()`, and changed item retrieval from `Chunk` to `Skip` and `Take` for more efficient pagination.

--- API/Day2/Lab/WebApplication1/WebApplication1/Extension/Extensions.cs
+++ API/Day2/Lab/WebApplication1/WebApplication1/Extension/Extensions.cs
@@ -7,15 +7,11 @@ namespace WebApplication1.Extension
         public static IEnumerable<O> Pagination<T, O>(this IEnumerable<T> source, IMapper _map, int page = 1, int pageSize = 10)
             where T : class where O : class
         {
-            if (source == null)
-                return [];
-            var size = source.Count();
-            pageSize = (pageSize < 1) ? 10 : pageSize;
-            int maxPages = (int)Math.Ceiling((double)size / pageSize);
-            page = Math.Max(1, page);
-            page = Math.Min(page, maxPages);
-            var students = source.Chunk(pageSize).ToList()[page - 1];
-            var studentsDTO = _map.Map<List<O>>(students);
+            if (source == null || pageSize < 1 || page < 1) return [];
+            int maxPages = (int)Math.Ceiling((double)source.Count() / pageSize);
+            if (page > maxPages) return [];
+            var studentsDTO = source.Skip((page - 1) * pageSize).Take(pageSize)
+                .Select(s => _map.Map<O>(s));
             return studentsDTO;
         }
 
