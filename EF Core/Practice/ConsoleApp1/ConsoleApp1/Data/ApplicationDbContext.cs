c025875e0a0fad3361726f62a154af7a71050fd5
Hesham749 <heesham749@gmail.com>
2025-03-10 01:44:26 +0200

    Initial commit for ConsoleApp1 project setup
    - Deleted binary file `lab1.rar`.
    - Added `Id` and `Author` properties in `Book.cs`.
    - Created solution file `ConsoleApp1.sln` and project file `ConsoleApp1.csproj` with necessary configurations.
    - Implemented `Program.cs` with a basic "Hello, World!" output.
    - Introduced `ApplicationDbContext` in `ApplicationDbContext.cs` for SQL Server configuration and defined `DbSet` properties for `Post` and `Tag`.
    - Created migration file `20250309230420_init.cs` to establish database schema for `Posts`, `Tags`, and `PostTag`.
    - Generated designer file for migration and created model snapshot for `ApplicationDbContext`.

---
+++ EF Core/Practice/ConsoleApp1/ConsoleApp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server = . ; database = efcoreTest ; integrated security = true; trust server certificate = true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Post>()
+                .HasData(new Post { PostId = 1, Content = "" } , new Post { PostId = 2, Content = "" });
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+        //public ICollection<PostTag> PostTags { get; set; }
+    }
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+        public ICollection<Post> Posts { get; set; }
+        //public ICollection<PostTag> PostTags { get; set; }
+
+    }
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
--- MVC/Day2/RepositoryPattern/RepositoryPattern.Core/Models/Book.cs
+++ MVC/Day2/RepositoryPattern/RepositoryPattern.Core/Models/Book.cs
@@ -5,11 +5,13 @@ namespace RepositoryPattern.Core.Models
 {
     public class Book
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required, StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
         [ForeignKey(nameof(Author))]
         public int AuthorId { get; set; }
+
         public virtual Author Author { get; set; }
     }
 }
