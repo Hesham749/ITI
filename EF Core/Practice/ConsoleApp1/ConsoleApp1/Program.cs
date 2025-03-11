9741c2b9d17e8d4685695e1348eb7104bd80667c
Hesham749 <heesham749@gmail.com>
2025-03-11 17:52:03 +0200

    init

--- EF Core/Practice/ConsoleApp1/ConsoleApp1/Program.cs
+++ EF Core/Practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace ConsoleApp1
+﻿using ConsoleApp1.Data;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            using var _context = new ApplicationDbContext();
+
+            var stock = _context.Posts.OrderByDescending(p => p.PostId);
+            Test(3);
         }
+
+
+
+
+
+
+
+
+
+
+
+        ///<summary> this is my test method </summary>
+        ///<param name="x"   >fasfasdf</param>
+        ///<returns>it will take int and it will print it.</returns>
+        ///<exception cref="ArgumentNullException"></exception>
+        static void Test(int x) => Console.WriteLine(x);
     }
 }
