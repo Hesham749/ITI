c7d111197a5237a74079664b15328282590ccfce
Hesham749 <heesham749@gmail.com>
2025-04-08 09:24:32 +0200

    feat: Add TestComponent and integrate PrimeNG themes

--- Angular/Day2/Lab/Demo02/src/app/app.component.ts
+++ Angular/Day2/Lab/Demo02/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from "./product/product.component";
 
+import { TestComponent } from "./test/test.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductComponent],
+  imports: [RouterOutlet, ProductComponent, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
